a315f3d4284479296a0e7bc6ff610f8b138ddbe4
MateoErazo <erazojesusmateo@gmail.com>
2022-04-12 17:28:03 -0500

    Agrego controlador Autor

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]//permite hacer validaciones automaticas respecto a la data recibida en nuestro controlador
+    [Route("api/autores")] //ruta del controlador
+    public class AutoresController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor> (){ 
+                new Autor() { ID = 1, Name = "Juan Lopez", Edad = 25},
+                new Autor() { ID = 2, Name = "Marcos Perez", Edad = 28},
+                new Autor() { ID = 3, Name = "Maria Antonieta", Edad = 28}
+            };
+        }
+        
+
+    }
+}
---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int Edad { get; set; }
+    }
+}
