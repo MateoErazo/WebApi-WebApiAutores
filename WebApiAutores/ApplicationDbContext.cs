2741aa9cf04b423e6242de10e852cbec09a4d064
MateoErazo <erazojesusmateo@gmail.com>
2022-04-12 20:33:19 -0500

    Instalo Entity Framework Core y creo tabla Autores desde EF

---
+++ WebApiAutores/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+     //con este constructor ya se le pueden pasar diferentes configuraciones para Entity Framework
+     //como el Connection String
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       public DbSet<Autor> Autores { get; set; }    
+    }
+}
---
+++ WebApiAutores/Migrations/20220413011954_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiAutores.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
