1e761c1dfdf8ba067ee93c1700773db0a44e0d31
MateoErazo <erazojesusmateo@gmail.com>
2022-04-29 06:55:29 -0500

    Implemento validaciones por defecto

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -1,11 +1,26 @@
-﻿namespace WebApiAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entidades
 {
     public class Autor
     {
         public int ID { get; set; }
-        public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(5,ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres")]
+        public string Nombre { get; set; }
+            
+        [Range(18,26)]
         public int Edad { get; set; }
+
+        [NotMapped]
+        [CreditCard]
+        public string TarjetaCredito { get; set; }
+
+        [NotMapped]
+        [Url]
+        public string DireccionUrl { get; set; }
 
         public List<Libro> Libros { get; set; } 
     }
