af866cbaa284a7c388a85addb1fbbe6c938de9ae
MateoErazo <erazojesusmateo@gmail.com>
2022-04-30 06:58:31 -0500

    Implemento validaciones personalizadas y por modelo

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -1,27 +1,54 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int ID { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(5,ErrorMessage ="El campo {0} no puede tener mas de {1} caracteres")]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
             
-        [Range(18,26)]
+        //[Range(18,26)]
         public int Edad { get; set; }
 
-        [NotMapped]
-        [CreditCard]
-        public string TarjetaCredito { get; set; }
+        //[NotMapped]
+        //[CreditCard]
+        //public string TarjetaCredito { get; set; }
 
-        [NotMapped]
-        [Url]
-        public string DireccionUrl { get; set; }
+        //[NotMapped]
+        //[Url]
+        //public string DireccionUrl { get; set; }
 
         public List<Libro> Libros { get; set; } 
+
+        [NotMapped]
+        public int Mayor { get; set; }
+        [NotMapped]
+        public int Menor { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                string primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                   yield return new ValidationResult("La primera letra debe ser mayuscula",
+                       new string[] {nameof(Nombre)} );
+                }
+            }
+
+            if (Menor>Mayor)
+            {
+                yield return new ValidationResult("El numero no puede ser mas grande que el mayor", 
+                    new string[] {nameof(Menor) });
+            }
+        }
     }
 }
